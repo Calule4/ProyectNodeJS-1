c07f99909949c24f9983f946eb9767f3ebaf9256
Sergio Aramayo <ser.aramayo@gmail.com>
2022-10-16 01:10:47 -0300

    Esperemos que ignore funcione y los package de node_modules no suban

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 // CREA servidor HTTP
-var http = require ('http'); //Importa el modulo HTTP solicitado
+// var http = require ('http'); //Importa el modulo HTTP solicitado
 
-http.createServer(function(req, res) { // crea el servidor http
-    res.writeHead(200, {'Content-Type': 'text/html'}); // establece tipo de contenido
-    res.end(' chanchito feliz '); // escribe contenido
-}).listen(7000); // escucha en el puerto 7000
+// http.createServer(function(req, res) { // crea el servidor http
+//     res.writeHead(200, {'Content-Type': 'text/html'}); // establece tipo de contenido
+//     res.end(' chanchito feliz '); // escribe contenido
+// }).listen(7000); // escucha en el puerto 7000
 
-console.log('Servidor ejemplo_1 corriendo en http://localhost:7000'); // para correrlo npm start en consola
+// console.log('Servidor ejemplo_1 corriendo en http://localhost:7000'); // para correrlo npm start en consola
+
+
+const express = require ('express'); //Importa funcion exprress
+const app = express (); //crea instancia express
+
+//GET/
+app.get('/', (req, res) => {    //request & response respectivamente
+  res.send('Hello LitlePig!')   //mensaje de salida
+})
+
+// Configura el puerto 
+
+app.listen(7000);    //escucha puerto 7000
+  console.log(`Example app listening on port 7000`);
+
+// Ejecutar comando npm start
